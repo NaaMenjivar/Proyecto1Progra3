472518432817f97067dd35b3280b1a0fa6116eb9
Naa <naara.menjivar.ramirez@est.una.ac.cr>
2025-09-11 23:51:27 -0600

    Revert "Dashoboard administrador"
    This reverts commit 196dc547f5183afc1ffbf92edafb27aae3613b47.

--- src/main/java/presentacion/vista/administrador/PanelDashboard.java
+++ src/main/java/presentacion/vista/administrador/PanelDashboard.java
@@ -1,25 +1,4 @@
 package presentacion.vista.administrador;
 
-import javax.swing.*;
-
 public class PanelDashboard {
-    private JPanel panelPrincipal;
-    private JComboBox desdeAnnioComboBox;
-    private JComboBox desdeMesComboBox;
-    private JComboBox hastaAnnioComboBox;
-    private JComboBox hastaMesComboBox;
-    private JComboBox medicamentosComboBox;
-    private JButton agregarUno;
-    private JButton agregarOtro;
-    private JButton quitarUno;
-    private JButton quitarOtro;
-    private JLabel desdeLbl;
-    private JLabel hastaLbl;
-    private JLabel medicamentosElegirLbl;
-    private JScrollPane listaMedicamentosFechas;
-    private JPanel panelGraficoLineas;
-    private JPanel panelGraficoPie;
-    private JPanel panelDatos;
-    private JPanel panelMedicamentosGraficoLineas;
-    private JPanel panelRecetas;
 }
